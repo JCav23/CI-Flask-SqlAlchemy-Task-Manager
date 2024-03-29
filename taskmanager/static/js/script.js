090836b094dd11f6ccdaefa00515696588b7b6ce
Jack Cavanagh <jack.m.cavanagh@hotmail.co.uk>
2024-03-29 16:06:16 +0000

    added lesson 8 material for task manager project

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -17,4 +17,8 @@ document.addEventListener('DOMContentLoaded', function() {
     // select initialization
     let selects = document.querySelectorAll('select');
     M.FormSelect.init(selects);
+
+    let collapsables = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsables);
   });
+
